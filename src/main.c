98e702fc952d70133852b4e854c30e20e112bfd6
AussieOssborn <osik013@gmail.com>
2023-10-20 18:42:41 +0300

    make

---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "parser.h"
+
+int main(int argc, char *argv[]) {
+    parser(argc, argv);
+    return 0;
+}
---
+++ src/parser.h
@@ -0,0 +1,7 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+
+int parser(int argc, char *argv[]);
+
+#endif // PARSER_H
