6a7b257e1121b7b1783ff1713aae8bbbd6248d79
erosrolf <erosrolf@gmail.com>
2023-10-20 20:08:32 +0300

    add moc cat func

---
+++ src/cat.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#ifndef CAT_H
+#define CAT_H
+
+void cat();
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,20 +1,21 @@
+#include "cat.h"
 #include "parser.h"
 
 int main(int argc, char *argv[]) {
-    char **fname = NULL;
-    int fcount = 0;
-    int first_fname = 0;
-    parser(argc, argv, &first_fname);
-    fcount = argc - first_fname;
-    if(fcount > 0){
-        fname = (char **)malloc(sizeof(char *) * fcount);
-    if(!fname)
-        exit(1);
-    for(int i = 0; i < fcount; i++){
-        fname[i] = argv[i + first_fname];
-        printf("%s\n", fname[i]);
-
+  char **fname = NULL;
+  int fcount = 0;
+  int first_fname = 0;
+  parser(argc, argv, &first_fname);
+  fcount = argc - first_fname;
+  if (fcount > 0) {
+    fname = (char **)malloc(sizeof(char *) * fcount);
+    if (!fname)
+      exit(1);
+    for (int i = 0; i < fcount; i++) {
+      fname[i] = argv[i + first_fname];
+      printf("%s\n", fname[i]);
     }
   }
-    return 0;
+  cat();
+  return 0;
 }
