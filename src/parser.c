09c7c0dee8ac57b1f7b31aadb5fbb691bb2b1725
erosrolf <erosrolf@gmail.com>
2023-10-24 17:39:19 +0300

    parser need fixed

--- src/parser.c
+++ src/parser.c
@@ -1,5 +1,6 @@
 #include "parser.h"
 
+// нужно создать структуру flag и в цикле while добавлять структуру
 int parser(int argc, char *argv[], int *first_fname) {
   int return_value = 0;
   int option_index = 0;
@@ -23,7 +24,40 @@ int parser(int argc, char *argv[], int *first_fname) {
     if (c == 't')
       printf("flag t\n");
   }
-   *first_fname = optind;
+  *first_fname = optind;
   return return_value;
 }
 
+// нужно доработать в функцию сортировки структур flag
+int main() {
+  int flags_len = 0;
+  int flag_c = 4;
+  char *flags = malloc(10);
+  struct flag {
+    char data;
+    int rang;
+  };
+
+  double arr[] = {1, 2, 3, 4, 5};
+
+  struct flag tmp[5];
+  tmp[0].data = 'b', tmp[0].rang = 0;
+  tmp[1].data = 'e', tmp[1].rang = 3;
+  tmp[2].data = 'n', tmp[2].rang = 2;
+  tmp[3].data = 's', tmp[3].rang = 1;
+  tmp[4].data = 't', tmp[4].rang = 4;
+  for (int i = 0; i <= 4; i++) {
+    for (int j = 0; j < flag_c; j++) {
+      if (tmp[j].rang == i) {
+        // printf("%c ", tmp[j].data);
+        flags[i] = tmp[j].data;
+        flags_len++;
+      }
+    }
+  }
+  flags[flags_len] = 0x00;
+  printf("arr size = %ld\n", sizeof(arr));
+  printf("struct size = %ld\n", sizeof(double));
+  // printf("%s\n", flags);
+  return 0;
+}
